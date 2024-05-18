9276cb24829887c95795f0b013494fa0e4d659f6
isacollazos <124264467+isacollazos@users.noreply.github.com>
2024-05-17 21:03:39 -0500

    Se agrego el registro de usuarios

--- src/Login/Register.jsx
+++ src/Login/Register.jsx
@@ -6,7 +6,6 @@ const Register = () => {
       <h1 className='text-center'>Registrar usuario</h1>
       <form className='formRegister'>
         <div className='form-group'>
-            <h1>Registar usuario</h1>
             <input type='text' className='nameRegister' placeholder='Nombre'/>
             <input type='email' className='emailRegister' placeholder='Email'/>
             <input type='password' className='passwordRegister' placeholder='Contraseña'/>
