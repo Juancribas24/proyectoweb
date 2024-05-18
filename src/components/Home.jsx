d58dea09b30a2efb91f6b39b5e874c37de53faf7
isacollazos <124264467+isacollazos@users.noreply.github.com>
2024-05-18 14:09:56 -0500

    Cambios en Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import appFirebase from '../credenciales/credenciales'
+import { getAuth, signOut } from 'firebase/auth'
 
-const Home = ({correoUser}) => {
+const auth = getAuth(appFirebase)
+
+const Home = ({ correoUser }) => {
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth)
+      console.log("Usuario deslogueado con éxito")
+    } catch (error) {
+      console.error("Error al desloguear usuario:", error)
+    }
+  }
+
   return (
     <div>
-        <h1>Bienvenido usuario {correoUser}</h1>
-      
+      <h1>Bienvenido usuario {correoUser} <button className='btn-logout' onClick={handleSignOut}>Logout</button></h1>
     </div>
   )
 }
 
 export default Home
+
