93afdd8330175806e7694725687e8cffd9674c72
isacollazos <124264467+isacollazos@users.noreply.github.com>
2024-05-18 12:30:16 -0500

    Cambios

--- src/Login/Register.jsx
+++ src/Login/Register.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
+import appFirebase from '../credenciales/credenciales'
+import { getAuth, createUserWithEmailAndPassword ,signInWithEmailAndPassword } from "firebase/auth"
+const auth = getAuth(appFirebase)
+
 
 const Register = () => {
+    const [register,setRegister] = useState(false)
+    
+
   return (
     <div className='containerRegister'>
       <h1 className='text-center'>Registrar usuario</h1>
@@ -9,8 +16,12 @@ const Register = () => {
             <input type='text' className='nameRegister' placeholder='Nombre'/>
             <input type='email' className='emailRegister' placeholder='Email'/>
             <input type='password' className='passwordRegister' placeholder='Contraseña'/>
-            <button className='buttonRegister'>Registarse</button>
+            <button className='btnRegister'>{register ?  'Registrarse' : 'Iniciar Sesión'}</button>
         </div>
+        <h4>{register ? 'Si ya tienes cuenta' : 'No tienes cuenta'} <button onClick={()=> setRegister( !register)} className='btnRegister'>{register ? 'Iniciar Sesión' : 'Registrarse'}</button></h4>
+
+
+
         
       </form>
     </div>
