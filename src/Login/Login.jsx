e769d6061178050ea8a7cdfae299a9f52ab1bf2b
isacollazos <124264467+isacollazos@users.noreply.github.com>
2024-05-18 12:13:54 -0500

    Se agrego el Login

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -3,36 +3,14 @@ import React from 'react'
 const Login = () => {
   return (
     <div>
-      <div className="login-container">
-      <h2>Iniciar Sesión</h2>
-      <form onSubmit={handleLogin} >
-        <div>
-          <label htmlFor="username">Usuario:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
-            placeholder="Usuario"
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor="password">Contraseña:</label>
-          <input
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder="Contraseña"
-            required
-          />
-        </div>
-        {error && <p className="error">{error}</p>}
-        <div className='divboton'>
-                <button type="submit" className="btn-sesion">Iniciar sesión</button>
-        </div>
-      </form>
+      <div className='login'>
+        <form>
+            <input type="text" className='email'placeholder='Ingresa Email'/>
+            <input type="password" className='password' placeholder='Ingresa Contraseña'/>
+            <button className='btnLogin'>Iniciar</button>
+        </form>
+        <h4><button className='btnRegister1'>Registrarse</button></h4>
+
       </div>
     </div>
   )
