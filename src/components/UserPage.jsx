e0b084e47b64e4281932329a3d08dc49a2dd8ff2
isacollazos <124264467+isacollazos@users.noreply.github.com>
2024-05-23 11:30:41 -0500

    Update UserPage.jsx

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from 'react';
+import appFirebase from '../credenciales/credenciales';
+import { getAuth, signOut } from 'firebase/auth';
 
-const UserPage = () => {
+const auth = getAuth(appFirebase);
+
+const UserPage = ({ correoUser }) => {
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      console.log("Usuario deslogueado con éxito");
+    } catch (error) {
+      console.error("Error al desloguear usuario:", error);
+    }
+  };
+
   return (
     <div>
-      
+      <h1>Bienvenido user {correoUser} <button className='btn-logout' onClick={handleSignOut}>Logout</button></h1>
     </div>
-  )
-}
+  );
+};
 
-export default UserPage
+export default UserPage;
